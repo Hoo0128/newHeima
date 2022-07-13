460076266ee7648b743252c744d066ae51a21b49
Hoo0128 <945174732@qq.com>
2022-07-13 15:36:59 +0800

    完成了初始化

---
+++ src/views/Login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <van-nav-bar
+      class="navbar"
+      title="登录"
+      left-text
+      left-arrow
+      @click-left="onClickLeft"
+    >
+      <template #left>  <van-icon name="cross" /> </template>
+    </van-nav-bar>
+  </div>
+</template>
+
+<script>
+import { Toast } from 'vant'
+export default {
+  methods: {
+    onClickLeft () {
+      Toast('返回')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.navbar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+  .van-icon {
+    color: #fff;
+  }
+}
+</style>
