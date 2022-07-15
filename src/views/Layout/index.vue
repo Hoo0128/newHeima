c3e0a66db8fb9fa458ad3b45a76c061fb734da06
Hoo0128 <945174732@qq.com>
2022-07-15 19:37:53 +0800

    login页面完成

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <router-view />
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <i class="toutiao toutiao-shouye"></i>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <i class="toutiao toutiao-shipin"></i>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <i class="toutiao toutiao-wenda"></i>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <i class="toutiao toutiao-wode"></i>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  .toutiao {
+    font-size: 0.53333rem;
+  }
+  .text {
+    font-size: 0.32rem;
+  }
+}
+</style>
